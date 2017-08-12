9f9cb66a100652d28fbadc101c51048e7e71d967
akashmore296 <akash.more296@gmail.com>
2017-08-12 22:17:22 +0530

    1 program

---
+++ com/Profound/util/SecondLargeSmall.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *  Purpose: Find Out 2nd largest and smallest no from array
+ *
+ *  @author  Akash More
+ *  @version 1.0
+ *  @since   11-08-2017
+ *
+ ******************************************************************************/
+
+package com.Profound.util;
+
+import java.util.Scanner;
+
+public class SecondLargeSmall
+{
+	
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[100];
+		System.out.println("Enter the no of elements");
+		int noOfElements=sc.nextInt();
+		System.out.println("Enter the elements of array");
+		for(int i=0;i<noOfElements;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(i=0;)
+		
+	
